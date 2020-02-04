a6687252aa9ac96d90ebdbbb40fbf07eefc1108a
Deepak Mane <deepak.mane123@gmail.com>
2020-02-03 23:40:30 -0800

    Created LinkedList_Implementation Program

---
+++ 03_LinkedLists/LinkedList_Implementation/src/LinkedList_Implementation.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : LinkedList_Implementation.c
+ Author      : Deepak Mane
+ Version     :
+ Copyright   : MIT
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
